62265551eb911e9239552cfaca35352bb6f94144
omar thamri <omarthamri4@gmail.com>
2018-11-25 00:30:21 +0100

    Add files via upload

---
+++ TinderSwipeAnimDemo/TinderSwipeAnimation.framework/Headers/TinderSwipeAnimation.h
@@ -0,0 +1,19 @@
+//
+//  TinderSwipeAnimation.h
+//  TinderSwipeAnimation
+//
+//  Created by MACBOOK PRO RETINA on 06/11/2018.
+//  Copyright © 2018 MACBOOK PRO RETINA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinderSwipeAnimation.
+FOUNDATION_EXPORT double TinderSwipeAnimationVersionNumber;
+
+//! Project version string for TinderSwipeAnimation.
+FOUNDATION_EXPORT const unsigned char TinderSwipeAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinderSwipeAnimation/PublicHeader.h>
+
+
